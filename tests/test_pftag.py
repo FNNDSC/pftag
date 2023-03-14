22bfa3a19de260ee2b664e68fd5190ad20b935a5
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-03-14 15:48:55 -0400

    Add noTags test

--- tests/test_pftag.py
+++ tests/test_pftag.py
@@ -7,9 +7,20 @@ import              pudb
 import              pytest
 from datetime       import datetime
 import              time
+from typing         import Any
 
 @pytest.fixture(params = ['literal', 'os'])
 def IOcomparisons_setup(request) -> tuple[str, str]:
+    """
+    Setup some straightforward inputs and expected output
+    results
+
+    Args:
+        request (_type_): internal pytest request
+
+    Returns:
+        tuple[str, str]: for each tag, return the input and expected result
+    """
     input:str
     output:str
     match request.param:
@@ -19,24 +30,31 @@ def IOcomparisons_setup(request) -> tuple[str, str]:
         case 'os':
             input   = r'This os is %os'
             output  = r'This os is posix'
+    print(f'Testing {input} transformed to {output}')
     return input, output
 
 def test_main(mocker, IOcomparisons_setup) -> None:
     """
     Simulated test run of the app using CLI.
     """
+    print(f'Testing IO comparisons')
     input:str           = IOcomparisons_setup[0]
     output:str          = IOcomparisons_setup[1]
-    mock_print          = mocker.patch('builtins.print')
+    mock_print:Any      = mocker.patch('builtins.print')
     main(['--tag', input])
     mock_print.assert_called_once_with(output)
 
-def test_noCLIinit() -> dict:
+def test_noCLIinit() -> None:
+    """
+    Test when initializing the tagger object with an empty dictionary,
+    i.e. simulating the case where no CLI is interpreted.
+    """
     # Here we create a tagger without the CLI parser
     # and run a tag lookup on a passed string. For the
     # sake of expediency, the original test string is
     # not transportable, so a simplified version is provided
     # but the original kept for reference
+    print(f'Testing init with no CLI')
     str_tagNonTransportable:str =  r'run-%timestamp_chrplc|:|-_-OS-%platform-%NAME_dcmname_-%literal_echo|Subject4,md5|5_.log'
     str_tag:str = r'run-%2023-03-12T17:17:40.997150-04:00_chrplc|:|-_-OS-Linux-%NAME_dcmname|Patient_-%literal_echo|Subject4,md5|5_.log'
     tagger:pftag.Pftag     = pftag.Pftag({})
@@ -44,20 +62,36 @@ def test_noCLIinit() -> dict:
     d_tag:dict             = tagger(str_tag)
     assert d_tag['result'] == "run-2023-03-12T17-17-40.997150-04-00-OS-Linux-ROSALES^ANDREW-37149.log"
 
+def test_noTags() -> dict:
+    """
+    Test the case when the input string has no tags. In
+    that case the output result is exactly the input.
+
+    Returns:
+        dict: _description_
+    """
+    print(f'Testing with string with no tags')
+    str_tag:str = r'http://192.168.1.200:22223/api/v1/object/collection/event'
+    tagger:pftag.Pftag      = pftag.Pftag({})
+    d_tag:dict              = tagger(str_tag)
+    assert d_tag['result']  == str_tag
+
 def test_timestamp_dt() -> None:
     """
     Get a %timestamp and assert return type is datetime.
     """
+    print(f'Testing timestamp_dt return')
     str_tag:str = r'%timestamp'
-    tagger:pftag.Pftag     = pftag.Pftag({})
-    d_tag:dict             = tagger(str_tag)
-    dt:datetime            = timestamp_dt(d_tag['result'])
-    assert type(dt)       == datetime
+    tagger:pftag.Pftag      = pftag.Pftag({})
+    d_tag:dict              = tagger(str_tag)
+    dt:datetime             = timestamp_dt(d_tag['result'])
+    assert type(dt)         == datetime
 
 def test_timestamp_diff() -> None:
     """
     Test a 1 second timestamp difference
     """
+    print(f'Testing 1 second time delay/track')
     tagger:pftag.Pftag      = pftag.Pftag({})
     d_tag:dict              = tagger(r'%timestamp')
     t1:str                  = d_tag['result']
